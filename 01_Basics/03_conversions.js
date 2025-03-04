bb06e49abc66c537831aecf7861e1a0f26d0e68d
ashishd881 <153411690+ashishd881@users.noreply.github.com>
2025-03-04 07:40:44 +0000

    conversion

---
+++ 01_Basics/03_conversions.js
@@ -0,0 +1,41 @@
+// let score=33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueINNumber =Number(score);   //score number me convert ho gaya
+
+
+// console.log(typeof (valueINNumber));   //ye no dega
+
+// let score1="33abc"
+// console.log(typeof score1);
+// let valueINNumber1 =Number(score1);
+// console.log(typeof (valueINNumber1));    //score1 ko number me convert kiya isliye no bata diya
+// console.log (valueINNumber1);      //nan ho gay par convert ho gaya score number me 
+
+// let score2=null
+// console.log(typeof score2);  //object
+// let valueINNumber2 =Number(score2);    // no me convert ho gaya
+// console.log(typeof (valueINNumber2));    //score2 ko number me convert kiya isliye no bata diya
+// console.log (valueINNumber2);          // 0 aa gayi 
+
+// score3=true
+// console.log(typeof score3);  
+// let valueINNumber3 =Number(score3);    // no me convert ho gaya
+// console.log(typeof (valueINNumber3));    //score3 ko number me convert kiya isliye no bata diya
+// console.log (valueINNumber3);           //1 aa gayi 
+
+
+//  let isloggedIn =""
+//  let booleanIsLoggedIn=Boolean(isloggedIn)
+//  console.log(booleanIsLoggedIn);   // false dega empty string
+
+//  let isloggedIn2 ="h"
+//  let booleanIsLoggedIn2=Boolean(isloggedIn2)
+//  console.log(booleanIsLoggedIn2);   //true dega string
+
+let somenumber=33
+let stringNumber = String(somenumber)
+console.log(stringNumber);   //dikhega no jaisa
+console.log(typeof(stringNumber));  //par yaha string print hoga kyunki convertr ho gaya
