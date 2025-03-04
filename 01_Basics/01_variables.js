56522ede995748a62643f7088138d0b5a34c8289
ashishd881 <153411690+ashishd881@users.noreply.github.com>
2025-03-04 07:00:37 +0000

    varables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =1234//semicilon lagao ya na lagao it is up to you
+let accountEmail="hitesh@google.com"  //let and var are two ways of variable declaration now let is used because scope of let is defined but not defined of var
+var accountPassword="1234"
+accountCity="Jaipur"   //not a good practice
+let accountState
+
+//accountId=2 const keyword change not allowed
+accountEmail="hc@hc.com"
+accountPassword="156234"
+accountCity="Banglore"
+
+console.log(accountId); //log likh ke enter mar do
+
+console.table([accountId,accountEmail,accountCity,accountState]) //saare variables ko ek saath print kar deta hai 
