2d8d5e3bc5ba931e83ba9ffea2df094c5e870594
ashishd881 <153411690+ashishd881@users.noreply.github.com>
2025-03-04 07:01:46 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";  //test all JS code as newer version
+
+// alert(3+3) //we are using nodejs,not browser browser undefined dega jab inspect me ja ke console me type karenge yaha error de raha 
+
+let name="hitesh"
+let age=18
+let isloggedIn=false
+
+//number is 2^53
+//bigint 
+//string "doublle quotes me"
+//boolean true/false
+//null is standalone value  jaise temperature kahi se aaya hi nahi toh null bol do because zero ek tmp hai
+//undefined 
+//symbol unique
+
+//object
+
+
+console.log( typeof"hitesh");//string
+
+console.log( typeof age);//number
+
+console.log( typeof"null");//string
+
+console.log( typeof"undefined");//string
+
+console.log( typeof null);  //object
+
+console.log( typeof undefined);    //undefined
