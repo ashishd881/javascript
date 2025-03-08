fd55f57a2f875247ce7da748326a395fcce8ed68
ashishd881 <153411690+ashishd881@users.noreply.github.com>
2025-03-08 12:45:16 +0000

    while loop

---
+++ 05_iterations/2_whileloop.js
@@ -0,0 +1,19 @@
+// let index=0;
+// while(index<=10){
+//     console.log(`value of index is ${index}`)
+//     index =index+2
+// }
+
+// let myArray=["flash","batman","superman"]
+// let arr=0
+// while(arr<myArray.length){
+//     console.log(`value is ${myArray[arr]}`)
+//     arr++
+// }
+
+
+let score=10
+do {
+    console.log(`score is ${score}`)
+    score=score+1
+} while (score<10);      //ye condition false hai par loop ek bar zarur chalega
